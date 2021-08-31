11e1a90e52168c7c1b3a0e2f80fe4a7c21e00925
Fabian <lindgrenfabian@gmail.com>
2021-09-01 00:09:39 +0200

    10/ Add ground check

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 3f;
-    public float jumpForce = 10f;
-
+    public float jumpForce = 2.5f;
+    public Transform groundCheck;
     private Rigidbody2D body;
 
     // Start is called before the first frame update
@@ -16,10 +16,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f);
+        bool isGrounded = false;
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.tag != "Player")
+            {
+                isGrounded = true; break;
+            }
+        }
+
         Vector3 moveBy = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         transform.Translate(moveBy * Time.deltaTime * speed);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            body.AddForce(new Vector2(0, jumpForce));
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            body.AddForce(new Vector2(0, jumpForce * 100));
     }
 }
