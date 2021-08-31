9691ed6ae02d1b0862ccf1c33bc4eb3842e2158d
Fabian <lindgrenfabian@gmail.com>
2021-08-31 23:49:07 +0200

    9/ Add jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 3f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D body;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Vector3 moveBy = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
         transform.Translate(moveBy * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            body.AddForce(new Vector2(0, jumpForce));
     }
 }
