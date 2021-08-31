d060dc4769e8179384c3abcb59313b16db737fda
Fabian <lindgrenfabian@gmail.com>
2021-08-31 18:49:55 +0200

    5/ Add basic movement
    lol

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveBy = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        transform.Translate(moveBy * Time.deltaTime);
+    }
+}
