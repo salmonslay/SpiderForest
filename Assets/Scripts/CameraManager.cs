974df5b98daeb813e61ae8fdcd21887d520abfa3
Fabian <lindgrenfabian@gmail.com>
2021-09-01 14:31:58 +0200

    16/ Add camera movement

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private Transform target;
+    public Vector3 offset = new Vector3(6, 0, -10);
+    public float smoothSpeed = 0.1f;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        float speed = smoothSpeed * Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, speed);
+    }
+}
