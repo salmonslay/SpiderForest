b89534685f49984cfb840961215d0c3da4c55679
Fabian <lindgrenfabian@gmail.com>
2021-09-08 17:11:37 +0200

    31/ Add climbing coin pickup audio

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,12 +2,30 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    /// <summary>
+    /// Total coins picked up during this level
+    /// </summary>
+    public static int PickedUp = 0;
+
+    private void Start()
+    {
+        PickedUp = 0;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            //play audio
+            GetComponent<AudioSource>().pitch = 1 + ((float)PickedUp++ / 30);
+            GetComponent<AudioSource>().Play();
+
             collision.GetComponent<PlayerState>().PickCoin(1);
-            Destroy(gameObject);
+
+            //destroy coin
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<CircleCollider2D>().enabled = false;
+            Destroy(gameObject, 10);
         }
     }
 }
