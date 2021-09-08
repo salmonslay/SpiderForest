e7b5dca9dd84958d7525533ff05d60cab20d07cd
Fabian <lindgrenfabian@gmail.com>
2021-09-08 20:58:54 +0200

    33/ Add custom ray speed

--- Assets/Scripts/Effects/BeatPulse.cs
+++ Assets/Scripts/Effects/BeatPulse.cs
@@ -4,17 +4,20 @@ using UnityEngine.UI;
 public class BeatPulse : MonoBehaviour
 {
     [SerializeField] private float BPM;
-    [SerializeField] private float size = 1.055f;
+
     [SerializeField] private GameObject ray;
+    [SerializeField] private float raySpeed = 30;
+
+    [SerializeField] private float logoSize = 1.055f;
     [SerializeField] private Color logoColor;
 
     private void Update()
     {
         var baseValue = Mathf.Cos(Time.time * Mathf.PI * (BPM / 60f) % Mathf.PI);
-        transform.localScale = Vector3.Lerp(new Vector3(1f, 1f, 1), new Vector3(size, size, 1), baseValue);
+        transform.localScale = Vector3.Lerp(new Vector3(1f, 1f, 1), new Vector2(logoSize, logoSize), baseValue);
         GetComponent<Image>().color = Color.Lerp(Color.white, logoColor, baseValue);
 
         //spin rays
-        ray.transform.Rotate(0, 0, 30 * Time.deltaTime);
+        ray.transform.Rotate(0, 0, 30 * Time.deltaTime * (Mathf.Abs(baseValue) + 1));
     }
 }
