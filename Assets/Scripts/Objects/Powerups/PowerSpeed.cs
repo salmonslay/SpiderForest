9f7e9d99776f3b0350e764eae3af72ab5d6003a0
Fabian <lindgrenfabian@gmail.com>
2021-09-11 19:52:18 +0200

    Comment power up

--- Assets/Scripts/Objects/Powerups/PowerSpeed.cs
+++ Assets/Scripts/Objects/Powerups/PowerSpeed.cs
@@ -6,9 +6,14 @@ public class PowerSpeed : PowerUp, IPowerUp
     public override IEnumerator Activate(Collider2D collision)
     {
         StartCoroutine(base.Activate(collision));
+
+        //speed up player & music
         collision.GetComponent<PlayerMovement>().speedModifier = 1.5f;
         GameObject.Find("Music").GetComponent<AudioSource>().pitch = 1.1f;
+
         yield return new WaitForSeconds(Duration);
+
+        //reset values
         collision.GetComponent<PlayerMovement>().speedModifier = 1f;
         GameObject.Find("Music").GetComponent<AudioSource>().pitch = 1f;
     }
