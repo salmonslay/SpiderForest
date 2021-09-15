e653d871219874e832daf0128c517f62608768fb
Fabian <lindgrenfabian@gmail.com>
2021-09-15 13:26:26 +0200

    Create base class for enemies

--- Assets/Scripts/Game/Enemy/Enemy.cs
+++ Assets/Scripts/Game/Enemy/Enemy.cs
@@ -1,57 +1,22 @@
 using UnityEngine;
 
+/// <summary>
+/// Base class for all enemies.
+/// </summary>
 public class Enemy : MonoBehaviour
 {
-    public float speed = 5f;
-    public float movementDirection = 1f;
-    public bool isAlive = true;
+    [HideInInspector] public bool isAlive = true;
+    [HideInInspector] public Rigidbody2D body;
+    [HideInInspector] public float movementDirection = 1f;
 
-    public Transform groundCheck;
-    private Rigidbody2D body;
-    private Animator animator;
+    /// <summary>
+    /// Damage this enemy will do on direct impact
+    /// </summary>
+    public int damage;
 
     private void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-    }
-
-    private void Update()
-    {
-        bool isGrounded = TouchesGround();
-        animator.SetBool("IsAlive", isAlive);
-        animator.SetBool("IsGrounded", isGrounded);
-
-        if (isAlive)
-        {
-            Vector3 moveBy = Vector3.zero;
-            moveBy.x = speed * Time.deltaTime * movementDirection;
-            transform.Translate(moveBy);
-
-            if (!isGrounded) ChangeDirection();
-        }
-    }
-
-    /// <summary>
-    /// Checks whether the enemy collides with another collider.
-    /// </summary>
-    private bool TouchesGround()
-    {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f);
-        foreach (Collider2D collider in colliders)
-        {
-            if (collider.gameObject != gameObject && !collider.isTrigger) return true;
-        }
-        return false;
-    }
-
-    /// <summary>
-    /// Change scale and movement direction of enemy
-    /// </summary>
-    public void ChangeDirection()
-    {
-        movementDirection *= -1;
-        transform.localScale = new Vector3(movementDirection, 1f);
     }
 
     /// <summary>
