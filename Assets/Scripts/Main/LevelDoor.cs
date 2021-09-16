1d07c96cf8fb39c969513144c6768ac056a2083b
Fabian <lindgrenfabian@gmail.com>
2021-09-16 21:32:46 +0200

    Add basic level doors

---
+++ Assets/Scripts/Main/LevelDoor.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelDoor : MonoBehaviour
+{
+    [Tooltip("Level this door will lead to upon interacting with it")]
+    [SerializeField] private Level level;
+
+    [Tooltip("Whether the player is touching this door or not")]
+    private bool isPlayerTouching = false;
+
+    [Tooltip("Audio clip to play when user tries to enter locked door")]
+    [SerializeField] private AudioClip lockedAudio;
+
+    private void Start()
+    {
+        if (level.Unlocked)
+        {
+            // todo: add something to show it
+        }
+    }
+
+    private void Update()
+    {
+        // handle interactions
+        if (Input.GetKeyDown(KeyCode.E) && isPlayerTouching)
+        {
+            // try to go to a level (through this door)
+            if (level.Unlocked)
+                Enter();
+            else
+                ShakeDoor();
+        }
+    }
+
+    /// <summary>
+    /// Open this door and go to the level/room in question
+    /// </summary>
+    private void Enter()
+    {
+        SceneManager.LoadScene(level.ID);
+    }
+
+    /// <summary>
+    /// Try to open this door but realize you can't.
+    /// So... you shake it instead :D right?
+    /// </summary>
+    private void ShakeDoor()
+    {
+        Helper.PlayAudio(lockedAudio);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) isPlayerTouching = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) isPlayerTouching = false;
+    }
+}
