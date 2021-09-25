87c564eebd966b613b4c06559d96f41342579980
Fabian <lindgrenfabian@gmail.com>
2021-09-25 21:24:16 +0200

    Fix elevator speed not mattering

--- Assets/Scripts/Objects/Elevator.cs
+++ Assets/Scripts/Objects/Elevator.cs
@@ -18,7 +18,7 @@ public class Elevator : MonoBehaviour
     private void Update()
     {
         Vector3 target = nodes[targetIndex].position;
-        transform.position = Vector2.MoveTowards(transform.position, target, 5 * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
         if (transform.position == target)
         {
             if (++targetIndex == nodes.Length) targetIndex = 0;
